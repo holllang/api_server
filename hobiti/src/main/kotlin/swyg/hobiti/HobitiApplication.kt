9d5bc7dddeea550ee53e01b6f469fcd7178c0fc2
CokeLee777 <akak640@kyonggi.ac.kr>
2023-01-24 20:35:23 +0900

    init: Add Spring project configuration
    스프링 프로젝트 환경설정 완료

---
+++ hobiti/src/main/kotlin/swyg/hobiti/HobitiApplication.kt
@@ -0,0 +1,11 @@
+package swyg.hobiti
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HobitiApplication
+
+fun main(args: Array<String>) {
+    runApplication<HobitiApplication>(*args)
+}
---
+++ hobiti/src/test/kotlin/swyg/hobiti/HobitiApplicationTests.kt
@@ -0,0 +1,13 @@
+package swyg.hobiti
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HobitiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
