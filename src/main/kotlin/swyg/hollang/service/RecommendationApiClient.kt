a717a2af06bffb23a7acc7a58d91a253b669c82b
CokeLee777 <akak640@kyonggi.ac.kr>
2023-02-18 02:15:16 +0900

    refactor: Openfeign url 동적으로 주입하게끔 변경

--- src/main/kotlin/swyg/hollang/service/RecommendationApiClient.kt
+++ src/main/kotlin/swyg/hollang/service/RecommendationApiClient.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import swyg.hollang.dto.CreateRecommendationResultResponse
 import swyg.hollang.dto.CreateTestResponseDetailRequest
 
-@FeignClient(name = "recommendationApiClient", url = "http://127.0.0.1:5000")
+@FeignClient(name = "recommendationApiClient", url = "\${inferring.server.host}:5000")
 interface RecommendationApiClient {
 
     @PostMapping("/test-responses", produces = [MediaType.APPLICATION_JSON_VALUE])
